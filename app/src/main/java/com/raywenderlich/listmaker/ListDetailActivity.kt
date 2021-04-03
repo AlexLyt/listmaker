531f9417a89e3386d6a0e4b838f96c492aa4572e
AlexLyt <aleks.zahid@gmail.com>
2021-04-03 16:11:46 -0400

    Completed chapter 9

--- app/src/main/java/com/raywenderlich/listmaker/ListDetailActivity.kt
+++ app/src/main/java/com/raywenderlich/listmaker/ListDetailActivity.kt
@@ -2,14 +2,22 @@ package com.raywenderlich.listmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class ListDetailActivity : AppCompatActivity() {
-    lateinit var list: TaskList
+
+    lateinit var listItemsRecyclerView : RecyclerView
+    var list: TaskList?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_list_detail)
+
+        list = intent.getParcelableExtra(MainActivity.INTENT_LIST_KEY)as TaskList?
+        title = list?.name
     }
+
 }
 
 
